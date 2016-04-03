c2647ef49c6db660d874da7ce9015bf27afd4359
Shafqat Dulal <shafqat.dulal@gmail.com>
2016-04-03 09:38:22 -0700

    Add Transaction to Charity

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403001557) do
+ActiveRecord::Schema.define(version: 20160403163436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,10 @@ ActiveRecord::Schema.define(version: 20160403001557) do
     t.string   "encrypted_user_id_iv"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
+    t.integer  "user_id"
   end
+
+  add_index "transactions", ["user_id"], name: "index_transactions_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "encrypted_email"
@@ -50,4 +53,5 @@ ActiveRecord::Schema.define(version: 20160403001557) do
     t.datetime "updated_at",         null: false
   end
 
+  add_foreign_key "transactions", "users"
 end
