76f6a40f2908dcd1c147fc1747050eed5e4d48a9
Shafqat Dulal <shafqat.dulal@gmail.com>
2016-04-03 09:36:00 -0700

    Add User to Transaction

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   attr_encrypted :data, key: :encryption_key
   attr_encrypted :name, key: :encryption_key
 
+  has_many :transactions
+
   def encryption_key
     'This is some user encrypted field'
   end
