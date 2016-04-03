6184a0f076acd5cfbf259215e38c54562677eff0
Kevin Lee <leekevin@berkeley.edu>
2016-04-03 08:45:26 -0700

    temp commit

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,14 +5,16 @@ class DashboardController < ApplicationController
   require 'coinbase/wallet'
 
   def index
-    @CLIENT_ID = "6500d9647dd187c6e8a5ab9a5f8a9b5c58156e67c4eeb4708a9fd1ce64ddda64"
-    @CLIENT_SECRET = "886b31a93be53f8c7d35ffc09048b38155dbfc763409bd81c23be1fddfb64c2f"
+    # @CLIENT_ID = "6500d9647dd187c6e8a5ab9a5f8a9b5c58156e67c4eeb4708a9fd1ce64ddda64"
+    # @CLIENT_SECRET = "886b31a93be53f8c7d35ffc09048b38155dbfc763409bd81c23be1fddfb64c2f"
+    @CLIENT_ID = '267ce9aec74165c0635151d76f4a2914b65da6b9f1598773886570490353ed1e'
+    @CLIENT_SECRET = '3cedba9b14371f826502fb5a0530bea1325c4eb6ecda08b6d80e6bd2e61c341a'
     @code = request.query_parameters()["code"]
     params = {"grant_type" => "authorization_code",
               "code" => @code,
               "client_id" => @CLIENT_ID,
               "client_secret" => @CLIENT_SECRET,
-              "redirect_uri" => "https://bitcoin-for-good.herokuapp.com/dashboard"}
+              "redirect_uri" => "http://127.0.0.1:3000/dashboard"}
 
 
     response = Net::HTTP.post_form(URI.parse("https://api.coinbase.com/oauth/token"), params)
@@ -24,7 +26,7 @@ class DashboardController < ApplicationController
   end
 
   def balance(request, token)
-
+    puts token
     client = Coinbase::Wallet::OAuthClient.new(access_token: token)
     account = client.primary_account
     @balance = account.balance.amount
